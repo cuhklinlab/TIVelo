c53e8acdee98fa7444ab6ea124fa44ec092d7644
Jishuai <aqianlikuaizaifeng@gmail.com>
2024-07-01 20:29:13 +0800

    0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="tivelo",
-    version="0.0.1",
+    version="0.0.3",
     description="single cell velocity analysis",
     package_dir={"": "tivelo"},
     packages=find_packages(where="tivelo"),
